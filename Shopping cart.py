cfa0fb8f8801f3de02bcef5b0774a511d11f91db
eugene <eugene.mogesa@strathmore.edu>
2025-06-09 09:38:31 +0300

    Recursion code

---
+++ Shopping cart.py
@@ -0,0 +1,20 @@
+class Shoppingcart:
+    def add_item(self,age,count):
+        self.age=age
+        self.count=count
+        print(f"{age}, {count}")
+
+obj1:Shoppingcart=Shoppingcart()
+obj1.add_item(20,1)
+obj1.add_item(self, age, count)
+self.boolI=True
+self.age=age
+self.count=count
+print(f"{self.age}, {self.count},{self.boolI}")
+
+obj1:Shoppingcart = Shoppingcart()
+obj2:Shoppingcart=Shoppingcart()
+obj2.boolI=False
+
+obj1.add_item(20,1)
+obj2.add_item(20,1)
